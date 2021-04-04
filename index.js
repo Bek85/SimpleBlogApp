32e1b23ad02fce490246fbb1bbb47c86f83eb4ce
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-04 20:08:37 +0500

    create "create.ejs" to receive POST request and save its contentto database

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const path = require('path');
 const app = new express();
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const BlogPost = require('./models/BlogPost');
 
-mongoose.connect('mongodb://localhost/my_database', { userNewUrlParser: true });
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -22,6 +31,17 @@ app.get('/contact', (req, res) => {
 
 app.get('/post', (req, res) => {
   res.render('post');
+});
+
+app.get('/posts/new', (req, res) => {
+  res.render('create');
+});
+
+app.post('/posts/store', (req, res) => {
+  // model creaates a new doc with browser data
+  BlogPost.create(req.body, (error, blogpost) => {
+    res.redirect('/');
+  });
 });
 
 app.listen(4000, () => {
