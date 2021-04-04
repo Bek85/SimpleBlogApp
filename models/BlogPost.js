b901b24289110bb4940efe58369aff87e180ed55
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-04 23:26:02 +0500

    add new fields to BlogPost model and render those fields

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,8 +4,13 @@ const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
   title: String,
   body: String,
+  username: String,
+  datePosted: {
+    /*can declare property type with an object like this because we need 'default' */ type: Date,
+    default: new Date(),
+  },
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
-console.log(BlogPost)
+console.log(BlogPost);
 module.exports = BlogPost;
